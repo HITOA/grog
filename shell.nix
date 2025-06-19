a569a49a53bd076c614c9aafdbd9ee6a2693227c
HITOA <charli.poirier@gmail.com>
2025-06-19 12:14:07 +0200

    Added iPlug2 cmake-new

--- shell.nix
+++ shell.nix
@@ -6,11 +6,15 @@ pkgs.mkShell {
     gdb
     pkg-config
 
+    alsa-lib
+    libGL
     libllvm
   ];
 
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc
+    alsa-lib
+    libGL
   ];
 
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";

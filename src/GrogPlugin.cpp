0e68d63ef8259baac00d679644d582b1ec4c21cf
HITOA <charli.poirier@gmail.com>
2025-06-20 07:44:34 +0200

    Added LICENSE + template plugin & ui

--- src/GrogPlugin.cpp
+++ src/GrogPlugin.cpp
@@ -3,8 +3,69 @@
 
 START_NAMESPACE_DISTRHO
 
+class GrogPlugin : public Plugin {
+public:
+    GrogPlugin() : Plugin{ 0, 0, 0 } {}
+
+protected:
+    const char* getLabel() const override {
+        return "Grog";
+    }
+
+    const char* getDescription() const override {
+        return "Modular node based synthesizer.";
+    }
+
+    const char* getMaker() const override {
+        return "HITO";
+    }
+
+    const char* getHomePage() const override {
+        return "https://github.com/HITOA/grog";
+    }
+
+    const char* getLicense() const override {
+        return "GPLv3";
+    }
+
+    uint32_t getVersion() const override {
+        return d_version(0, 0, 1);
+    }
+
+    void initAudioPort(bool input, uint32_t index, AudioPort& port) override {
+        port.groupId = kPortGroupStereo;
+        Plugin::initAudioPort(input, index, port);
+    }
+    
+    void initParameter(uint32_t index, Parameter& parameter) override {
+
+    }
+
+    float getParameterValue(uint32_t index) const override {
+        return 0.0f;
+    }
+
+    void setParameterValue(uint32_t index, float value) override {
+
+    }
+
+
+    void run(const float** inputs, float** outputs, uint32_t frames,
+                     const MidiEvent* midiEvents, uint32_t midiEventCount) override {
+        
+        for (uint32_t i = 0; i < frames; ++i) {
+            outputs[0][i] = 0;
+            outputs[1][i] = 0;
+        }
+    }
+
+
+private:
+    DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GrogPlugin)
+};
+
 Plugin* createPlugin() {
-    return nullptr;
+    return new GrogPlugin{};
 }
 
 END_NAMESPACE_DISTRHO
--- src/GrogUI.cpp
+++ src/GrogUI.cpp
@@ -3,8 +3,31 @@
 
 START_NAMESPACE_DISTRHO
 
+class GrogUI : public UI {
+public:
+    GrogUI() : UI{ DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT } {
+
+    }
+
+protected:
+    void parameterChanged(uint32_t index, float value) override {
+
+    }
+
+    void onDisplay() override {
+        
+    }
+
+    bool onMouse(const MouseEvent& event) override {
+        return false;
+    }
+
+private:
+    DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GrogUI)
+};
+
 UI* createUI() {
-    return nullptr;
+    return new GrogUI{};
 }
 
 END_NAMESPACE_DISTRHO
